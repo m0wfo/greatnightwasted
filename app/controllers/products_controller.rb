25ceec19f7b45599842ab26b27cef5868b9bf47d
Chris Mowforth <chris@mowforth.com>
2009-04-01 22:37:38 +0100

    Only show products in stock

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   make_resourceful do
     actions :index, :show
   end
+  
+  protected
+  
+  def current_objects
+    @current_objects ||= Product.in_stock
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ActiveRecord::Base
   
   has_many :items
   has_many :orders, :through => :items
+  
+  named_scope :in_stock, :conditions => ["quantity > 0"]
 end
